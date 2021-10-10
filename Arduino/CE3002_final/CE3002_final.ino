54cb8b8424ec6139b315ba83299958d7041fe9e2
moderatelyokay <56947568+moderatelyokay@users.noreply.github.com>
2021-10-10 15:59:10 +0800

    uploaded arduino code

---
+++ Arduino/CE3002_final/CE3002_final.ino
@@ -0,0 +1,311 @@
+/* 
+  Super Mario Bros - Overworld theme 
+  Connect a piezo buzzer or speaker to pin 11 or select a new pin.
+  More songs available at https://github.com/robsoncouto/arduino-songs                                            
+                                              
+                                              Robson Couto, 2019
+*/
+//hi
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+// change this to make the song slower or faster
+int tempo = 200;
+
+int buzzer = 11;
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Super Mario Bros theme
+  // Score available at https://musescore.com/user/2123/scores/2145
+  // Theme by Koji Kondo
+  
+  
+  NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,8, //1
+  NOTE_G5,4, REST,4, NOTE_G4,8, REST,4, 
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 3
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 3
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+
+   
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+
+//********************************************************************
+
+
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
+
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,20,4) for 20x4 LCD.
+
+char state = 0;
+
+//********************************************************************
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(1200); //for finger thing
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+  pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+  //marioSound();
+
+  //welcomeMsg();
+
+  
+
+ Serial.begin(9600); 
+}
+
+int sig;
+int bpm;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  sig=analogRead(A1);
+  Serial.println(sig);
+
+  if(Serial.available() > 0)
+  
+  { // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+
+    if (state == '1') { //if phone sends char 1 then switch on 
+      digitalWrite(7, LOW); //turn off RED LED
+      lcd.clear();
+      digitalWrite(5, HIGH); // Turn GREEN LED
+      Serial.println("LED: ON"); // Send back, to the phone, the String "LED: ON"
+      marioSound();
+      welcomeMsg();
+
+      if ( bpm > 400 && bpm <= 500 )
+      {
+        void calcMsg();
+        delay(2000);
+        //you are okay
+        bpmMsg();
+        
+      }
+
+      else if (bpm > 600 && bpm <= 800 )
+      {
+         void calcMsg();
+         delay(2000);
+        //take a breather
+        bpmMsg();
+      }
+
+      else if (bpm > 900)
+      {
+        void calcMsg();
+         delay(2000);
+         bpmMsg();
+        //are u ok?
+      }
+
+      else if (sig < 400)
+      {
+        void fingerMsg();
+        
+      }
+     
+    }
+
+
+    
+  }
+
+
+
+
+
+}
+
+
+void welcomeMsg(){
+  lcd.setCursor(2, 0); // Set the cursor on the third column and first row.
+  lcd.print("PLEASE PLACE"); // Print the string "Hello World!"
+  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("YOUR FINGER!");
+  delay(5000);
+
+}
+
+
+void calcMsg(){
+  lcd.setCursor(0, 0); // Set the cursor on the third column and first row.
+  lcd.print("CALCULATING YOUR"); // Print the string "Hello World!"
+  lcd.setCursor(1, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("BPM. PLEASE HOLD.");
+}
+
+void bpmMsg(){
+  lcd.setCursor(0, 0); // Set the cursor on the third column and first row.
+  lcd.print("YOUR BPM IS:"); // Print the string "Hello World!"
+  lcd.setCursor(1, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("");
+}
+
+void errorMsg(){
+  // Print 'Hello noWorld' on the first line of the LCD:
+  lcd.setCursor(0, 0); // Set the cursor on the third column and first row.
+  lcd.print("NOT CONNECTED"); // Print the string "Hello World!"
+  lcd.setCursor(0, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("TO PHONE");
+}
+
+void heartbeatSound(){
+    int i = 0;
+
+  do{
+    i++;
+    tone(buzzer, 1000); //pin 11, 450hz square frequency
+    delay(200); //500 millis
+    noTone(buzzer); //'free pin 11'
+    delay(200); //500 millis
+  }
+  while(i<2);
+
+  delay(500);
+}
+
+void marioSound(){
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
+
+
+}
